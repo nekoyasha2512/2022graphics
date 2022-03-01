b5c4a0c087b64e949a74d4f10ac160be37c0de50
unknown <inuyasha089487@gmail.com>
2022-03-01 12:02:06 +0800

    add week02

---
+++ week02/main.cpp
@@ -0,0 +1,32 @@
+#include <GL/glut.h>
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT |GL_DEPTH_BUFFER_BIT);
+
+    glColor3f(1, 1, 0);
+   /// glutSolidTeapot(0.3);
+
+    glBegin(GL_POLYGON);
+        glColor3f(1, 0, 0);
+        glVertex2f(-1, -1);
+
+        glColor3f(0, 1, 0);
+        glVertex2f(+1, -1);
+
+        glColor3f(0, 0, 1);
+        glVertex2f(0, +1);
+    glEnd();
+
+    glutSwapBuffers();
+}
+
+int main(int argc, char** argv)
+{
+    glutInit( &argc, argv);
+    glutInitDisplayMode( GLUT_DOUBLE | GLUT_DEPTH );
+    glutCreateWindow("week_02");
+
+    glutDisplayFunc(display);
+    glutMainLoop();
+}
